8142ab095c4cdbc07f7be00c51b21aee27569b97
medrimonia <medrimonia@gmail.com>
2016-01-15 15:11:04 +0900

    Now ip_random_controller read joint states and plot the appropriate values

--- src/ip_random_controller.cpp
+++ src/ip_random_controller.cpp
@@ -1,4 +1,5 @@
 #include <control_bridge.hpp>
+#include <joint_listener.hpp>
 
 #include <ros/ros.h>
 
@@ -12,26 +13,49 @@ int main(int argc, char ** argv)
   ros::init(argc, argv, "ip_random_controller");
   ros::NodeHandle nh;
 
-  std::cout << "Namespace: " << ros::this_node::getNamespace() << std::endl;
+  std::cerr << "Namespace: " << ros::this_node::getNamespace() << std::endl;
   
   // Random control properties
   int frequency = 10;//Hz
-  double maxTorque = 3;//N*m
+  double maxTorque = 2;//N*m
   std::string dof = "axis";
   
   // INITIALIZATION
   ros::Rate r(frequency);
   // Communications with controllers
-  ControlBridge bridge(nh, {dof}, ros::this_node::getNamespace() + "/");
+  std::string ns = ros::this_node::getNamespace();
+  ControlBridge bridge(nh, {dof}, ns + "/");        // Write command
+  JointListener listener(nh, ns + "/joint_states"); // Read status
   // Random
   unsigned long seed = std::chrono::system_clock::now().time_since_epoch().count();
   std::default_random_engine generator(seed);
   std::uniform_real_distribution<double> distribution(-maxTorque,maxTorque);
 
+
+  // csv header
+  std::cout << "time,pos,vel,eff,cmd" << std::endl;
+
+  double cmd = 0;
+
   while (ros::ok())
   {
-    double value = distribution(generator);
-    bridge.send({{dof,value}});
+    // Read value
+    auto joints = listener.getStatus();
+    try {
+      JointListener::JointState state = joints.at(dof);
+      state.pos = fmod(state.pos, 2 * M_PI);
+      std::cout << ros::Time::now().toSec() << ","
+                << state.pos << ","
+                << state.vel << ","
+                << state.eff << ","
+                << cmd << std::endl;
+    }
+    catch (const std::out_of_range &exc) {
+      std::cerr << "Failed to find " << dof << " in the published joints" << std::endl;
+    }
+
+    cmd = distribution(generator);
+    bridge.send({{dof,cmd}});
     ros::spinOnce();
     r.sleep();
   }
